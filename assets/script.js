5daf223bc72b864406df42c1cb482dd0e96ea1d6
Nick S <nicholasstacy89@gmail.com>
2022-11-05 20:13:43 -0400

    added javascript code and updated html

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,26 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomtopic = topics[Math.floor(Math.random() * topics.length)];
+function listtopics() {
+  for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+ }
 }
 
-
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
+function selecttopic() {
+  if (randomtopic === 'HTML') {
+  console.log("Let's study HTML!");
+}  else if (randomtopic === 'CSS') {
+  console.log("Let's study CSS!");
+}  else if (randomtopic === 'Git') {
+  console.log("Let's study Git!");
+}  else if (randomtopic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+}  else {
  console.log('Please try again!');
 }
-*/
+}
+console.log('Here are the topics we learned through Prework:');
+listtopics();
+console.log('Which topic should we study first?');
+selecttopic();
